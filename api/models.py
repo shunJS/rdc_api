2cb9be1e1f474201f4007dc08f607ac01e32d996
[shunJS] <aoooooososhun@gmail.com>
2020-12-27 01:41:36 +0900

    usermodel 修正

--- api/models.py
+++ api/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-
 class UserManager(BaseUserManager):
     def create_user(self, email, date_of_birth, password=None):
         """
--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,19 @@
+
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Task
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('id','email','password')
+        extra_kwargs= {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(**validated_data)
+        return user
 
 
 
